d150bee593865cea65e0c5b212cd0150a8587cbc
RAFZ TECH <126894899+rafaaaelrodrigues@users.noreply.github.com>
2026-05-04 10:41:53 -0300

    ss

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 // 🔥 IMPORTANTE
-import './styles/index.css'
+import '../src/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
