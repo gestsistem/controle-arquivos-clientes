97f3c62b80f7936dd1a15407af089c2ce8e361af
RAFZ TECH <126894899+rafaaaelrodrigues@users.noreply.github.com>
2026-05-04 11:42:55 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import App from "./app/App"
+import "./styles/index.css"
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const root = document.getElementById("root")
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (root) {
+  createRoot(root).render(<App />)
+}
